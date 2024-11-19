246f0dbf5f177acd0965ad7b581dab9987a04da0
aliimI <aliim12025@gmail.com>
2024-11-19 16:52:45 +0500

    added cors middleware

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Query, Depends
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from datetime import date
 from typing import Optional
@@ -20,3 +21,16 @@ app.include_router(router_bookings)
 app.include_router(router_pages)
 app.include_router(router_hotels)
 app.include_router(router_images)
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers",
+                   "Access-Control-Allow-Origin", "Authorization"]
+)
